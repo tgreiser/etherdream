fec57f280b4cff725dea8c264d7d6e1347680b78
tgreiser <tgreiser@gmail.com>
2016-10-22 23:55:28 -0700

    Pre alpha functional state

--- examples/connect/connect.go
+++ examples/connect/connect.go
@@ -1,3 +1,21 @@
+/*
+# Copyright 2016 Tim Greiser
+# Based on work by Jacob Potter, some comments are from his
+# protocol documents
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -10,7 +28,7 @@ func main() {
 	log.Printf("Listening...\n")
 	addr, bp, err := etherdream.FindFirstDAC()
 	if err != nil {
-		log.Fatal("Network error: %v", err)
+		log.Printf("Network error: %v", err)
 	}
 
 	log.Printf("Found DAC at %v\n", addr)
@@ -18,8 +36,7 @@ func main() {
 	log.Printf("BP:\n%v\n", bp)
 	log.Printf("Status:\n%v\n", bp.Status)
 
-	dac := etherdream.NewDAC(addr.IP.String())
-	err = dac.Init()
+	dac, err := etherdream.NewDAC(addr.IP.String())
 	if err != nil {
 		log.Fatal(err)
 	}
